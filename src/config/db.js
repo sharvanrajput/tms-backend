fa265356771c972fa816cdcfb4cfa22fc5df1e24
sharvanrajput <sharvanrajput098@gmail.com>
2025-11-25 19:20:36 +0530

    spacing

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const ConnectDb = async (req, res) => {
   try {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL+"/tmsdb")
     console.log("database connected successfully")
   } catch (error) {
     console.log("database connected successfully")
