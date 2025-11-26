4e1f4d00c9d41658c5382282cfbcf2b3c54416e0
sharvanrajput <sharvanrajput098@gmail.com>
2025-11-26 09:26:52 +0530

    db remove

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const ConnectDb = async (req, res) => {
   try {
-    mongoose.connect(process.env.MONGO_URL+"/tmsdb")
+    mongoose.connect(process.env.MONGO_URL)
     console.log("database connected successfully")
   } catch (error) {
     console.log("database connected successfully")
