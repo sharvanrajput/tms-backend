d7da95827cca7583181fd747d20af5dea3d5e625
sharvanrajput <sharvanrajput098@gmail.com>
2025-11-26 09:22:32 +0530

    update CORS configuration to allow requests only from localhost:5173a

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 dotenv.config()
   
 app.use(cors({
-  origin: "*"
+  origin: "http://localhost:5173"
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
